46ec08889fa8879cfdeaba619fb4b2f564f6b609
Malcolm Parry <malckinator@gmail.com>
2026-01-19 16:43:33 +0000

    setup boilerplate

--- src/Renderer.zig
+++ src/Renderer.zig
@@ -5,6 +5,13 @@ const math = mw.math;
 
 event_queue: mw.EventQueue,
 window: mw.Window,
+instance: gpu.Instance,
+device: gpu.Device,
+display: gpu.Display,
+image_available_semaphore: gpu.Semaphore,
+render_finished_semaphore: gpu.Semaphore,
+presented_fence: gpu.Fence,
+cmd_encoder: gpu.CommandEncoder,
 
 pub fn init(this: *@This(), alloc: std.mem.Allocator) !void {
     this.event_queue = try .init(alloc);
@@ -12,22 +19,81 @@ pub fn init(this: *@This(), alloc: std.mem.Allocator) !void {
 
     this.window = try .init(alloc, "malcraft", .{ 100, 100 }, &this.event_queue);
     errdefer this.window.deinit();
+
+    this.instance = try .init(true, alloc);
+    errdefer this.instance.deinit(alloc);
+
+    const phys_device = try this.instance.bestPhysicalDevice();
+    this.device = try .init(this.instance, phys_device, alloc);
+    errdefer this.device.deinit(alloc);
+
+    this.display = try .init(this.device, &this.window, alloc);
+    errdefer this.display.deinit(alloc);
+
+    this.image_available_semaphore = try .init(this.device);
+    errdefer this.image_available_semaphore.deinit(this.device);
+
+    this.render_finished_semaphore = try .init(this.device);
+    errdefer this.render_finished_semaphore.deinit(this.device);
+
+    this.presented_fence = try .init(this.device, true);
+    errdefer this.presented_fence.deinit(this.device);
+
+    this.cmd_encoder = try .init(this.device);
+    errdefer this.cmd_encoder.deinit(this.device);
 }
 
 pub fn deinit(this: *@This(), alloc: std.mem.Allocator) void {
+    this.device.waitUntilIdle();
+    this.cmd_encoder.deinit(this.device);
+    this.presented_fence.deinit(this.device);
+    this.render_finished_semaphore.deinit(this.device);
+    this.image_available_semaphore.deinit(this.device);
+    this.display.deinit(alloc);
+    this.device.deinit(alloc);
+    this.instance.deinit(alloc);
     this.window.deinit();
     this.event_queue.deinit();
-    _ = alloc;
 }
 
-pub fn render(this: *@This(), alloc: std.mem.Allocator) !bool {
+pub fn render(this: *@This(), alloc: std.mem.Allocator) !void {
     _ = alloc;
-    this.window.update();
-    while (this.event_queue.pending()) {
-        switch (this.event_queue.pop()) {
-            else => {},
-        }
-    }
+    try this.presented_fence.wait(this.device, std.time.ns_per_s);
+    try this.presented_fence.reset(this.device);
 
-    return !this.window.shouldClose();
+    const image_index = switch (try this.display.acquireImageIndex(this.image_available_semaphore, null, std.time.ns_per_s)) {
+        .success => |x| x,
+        .suboptimal => |x| x,
+        .out_of_date => return error.Failed,
+    };
+
+    try this.cmd_encoder.begin(this.device);
+    this.cmd_encoder.cmdMemoryBarrier(this.device, &.{.{
+        .image = .{
+            .image = this.display.image(image_index),
+            .old_layout = .undefined,
+            .new_layout = .color_attachment,
+            .src_stage = .{ .pipeline_start = true },
+            .dst_stage = .{ .color_attachment_output = true },
+            .src_access = .{},
+            .dst_access = .{ .color_attachment_write = true },
+        },
+    }});
+
+    this.cmd_encoder.cmdMemoryBarrier(this.device, &.{
+        .{ .image = .{
+            .image = this.display.image(image_index),
+            .old_layout = .color_attachment,
+            .new_layout = .present_src,
+            .src_stage = .{ .color_attachment_output = true },
+            .dst_stage = .{ .pipeline_end = true },
+            .src_access = .{ .color_attachment_write = true },
+            .dst_access = .{},
+        } },
+    });
+
+    try this.cmd_encoder.end(this.device);
+    try this.cmd_encoder.submit(this.device, &.{this.image_available_semaphore}, &.{this.render_finished_semaphore}, null);
+
+    _ = try this.display.presentImage(image_index, &.{this.render_finished_semaphore}, this.presented_fence);
 }
--- src/main.zig
+++ src/main.zig
@@ -7,5 +7,17 @@ pub fn main() !void {
     var app: App = undefined;
     try app.init(alloc);
     defer app.deinit(alloc);
-    while (try app.renderer.render(alloc)) {}
+    const renderer = &app.renderer;
+    while (true) {
+        try renderer.render(alloc);
+
+        renderer.window.update();
+        while (renderer.event_queue.pending()) {
+            switch (renderer.event_queue.pop()) {
+                else => {},
+            }
+        }
+
+        if (renderer.window.shouldClose()) break;
+    }
 }
