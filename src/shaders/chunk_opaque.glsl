7e2d67a0e6eef0106553d1e2bcacb27690ae5ed1
Malcolm Parry <me@mparry.dev>
2026-02-18 15:07:09 +0000

    improved shader

--- src/shaders/chunk_opaque.glsl
+++ src/shaders/chunk_opaque.glsl
@@ -3,18 +3,18 @@
 
 #include "core.hglsl"
 
-layout(location = 0) toPixel flat uint pPacked;
-layout(location = 1) toPixel float pAo;
-layout(location = 2) toPixel vec2 pUvs;
+layout(location = 0) toPixel flat uint p_packed;
+layout(location = 1) toPixel float p_ao;
+layout(location = 2) toPixel vec2 p_uvs;
 
 #ifdef _VERTEX
 
-layout(location = 0) in uvec2 iPacked;
+layout(location = 0) in uvec2 i_packed;
 
 layout(push_constant) uniform PushConstants {
     mat4 vp;
     ivec3 chunk_pos;
-} constants;
+} pc;
 
 const ivec3 face_origin_table[6] = {
     ivec3(1, 1, 0),
@@ -57,9 +57,9 @@ const uint ao_index_table[6] = {
 
 const float ao_table[4] = {
     1.00,
+    0.80,
+    0.60,
     0.40,
-    0.20,
-    0.10,
 };
 
 const vec2 uv_table[6] = {
@@ -79,8 +79,8 @@ const uint vindex_table[6 * 2] = {
 };
 
 void main() {
-    uint lower = iPacked.x;
-    uint upper = iPacked.y;
+    uint lower = i_packed.x;
+    uint upper = i_packed.y;
 
     int w = int(((lower >> 18) & 0x1f) + 1);
     int h = int(((lower >> 23) & 0x1f) + 1);
@@ -94,28 +94,28 @@ void main() {
     );
 
     uint vindex = vindex_table[gl_VertexIndex + flipped * 6];
-    ivec3 block_pos = rel_pos + constants.chunk_pos * 32;
+    ivec3 block_pos = rel_pos + pc.chunk_pos * 32;
     ivec3 face_origin = face_origin_table[face];
     ivec3 right_offset = w * right_table[vindex] * face_right_table[face];
     ivec3 up_offset = h * up_table[vindex] * face_up_table[face];
     ivec3 face_pos = face_origin + right_offset + up_offset;
 
-    gl_Position = constants.vp * vec4(face_pos + block_pos, 1);
-    pPacked = face | (tex_id << 3);
+    gl_Position = pc.vp * vec4(face_pos + block_pos, 1);
+    p_packed = face | (tex_id << 3);
 
-    pAo = ao_table[(upper >> ao_index_table[vindex]) & 3];
+    p_ao = ao_table[(upper >> ao_index_table[vindex]) & 3];
 
     vec2 base_uv = uv_table[vindex];
     vec2 repeat = ivec2(w, h);
-    pUvs = base_uv * repeat;
+    p_uvs = base_uv * repeat;
 }
 
 #endif
 
 #ifdef _PIXEL
 
-layout(binding = 0) uniform sampler2DArray uSampler;
-layout(location = 0) out vec4 oColor;
+layout(binding = 0) uniform sampler2DArray u_sampler;
+layout(location = 0) out vec4 o_color;
 
 const vec3 sun_dir = normalize(vec3(-1, -2, 5));
 
@@ -129,13 +129,13 @@ const float light_lookup[6] = {
 };
 
 void main() {
-    uint face = pPacked & 0x7;
-    uint tex_id = pPacked >> 3;
+    uint face = p_packed & 0x7;
+    uint tex_id = p_packed >> 3;
 
-    float light = light_lookup[face] * pAo;
+    float light = light_lookup[face] * (p_ao * p_ao);
 
-    vec4 albedo = texture(uSampler, vec3(pUvs, tex_id));
-    oColor = albedo * light;
+    vec4 albedo = texture(u_sampler, vec3(p_uvs, tex_id));
+    o_color = albedo * light;
 }
 
 #endif
