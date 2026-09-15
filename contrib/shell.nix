0497c1082c0001a831bd29cc3a5b13f3e68b4026
Malcolm Parry <me@mparry.dev>
2026-09-15 13:07:28 +0100

    fixed compiler segfault

--- contrib/shell.nix
+++ contrib/shell.nix
@@ -2,19 +2,9 @@
   pkgs ? import <nixpkgs> { },
 }:
 
-let
-  nixpkgs-26-05 = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-26.05.tar.gz";
-  };
-
-  pkgs-26-05 = import nixpkgs-26-05 {
-    inherit (pkgs) system;
-  };
-
-in
 pkgs.mkShell {
   packages = [
-    pkgs-26-05.zig_0_16
+    pkgs.zig_0_16
     pkgs.libx11
     pkgs.vulkan-loader
     pkgs.vulkan-validation-layers
